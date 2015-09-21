726708682ec78751a37ff950fd5b0ce7306699f8
Simon Perkins <simon.perkins@gmail.com>
2015-09-21 13:23:42 +0200

    Swap base.sh import position.
    The `module load` commands seem to overwrite
    LD_LIBRARY_PATH, instead of adding to it.

--- test_mb.sh
+++ test_mb.sh
@@ -1,15 +1,16 @@
 #PBS -q GPUQ
 #PBS -l nodes=srvslsgpu003:ppn=4:seriesGPUk
-#PBS -N compile-montblanc
+#PBS -N test-montblanc
 #PBS -o Q.test-mb.log
 #PBS -e Q.test-mb.err
 #PBS -V
 
-source $HOME/scripts/base.sh
 
-module load compilers/gcc492
+module load compilers/gcc484
 module load mpi/openmpi-1.8.8
 module load cuda/Cuda-6.5
+
+source $HOME/scripts/base.sh
 
 export CUDA_DEVICE=0
 #cd ${SOURCE_PREFIX}/montblanc/montblanc/tests
