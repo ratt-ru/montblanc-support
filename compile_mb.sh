11ed2bb6698f690384e1308f78a5d1efb45ed61d
Simon Perkins <simon.perkins@gmail.com>
2015-09-21 09:44:57 +0200

    Add montblanc compilation script.

---
+++ compile_mb.sh
@@ -0,0 +1,15 @@
+#PBS -q GPUQ
+#PBS -l nodes=srvslsgpu003:ppn=4:seriesGPUk
+#PBS -N compile-montblanc
+#PBS -o Q.compile-mb.log
+#PBS -e Q.compile-mb.err
+#PBS -V
+
+source $HOME/scripts/base.sh
+
+module load compilers/gcc492
+module load mpi/openmpi-1.8.8
+module load cuda/Cuda-6.5
+
+cd ${SOURCE_PREFIX}/montblanc
+python setup.py install
