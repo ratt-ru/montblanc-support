9e38f7c2216b032215183960571f037842fef4fe
Simon Perkins <simon.perkins@gmail.com>
2015-09-22 14:25:27 +0200

    Fixup source prefix paths in python installs.
    The script was looking in the wrong place
    for the SSL certificate, and there was
    a typo in the source prefix directory variable.

--- install_python_pkg.sh
+++ install_python_pkg.sh
@@ -11,18 +11,21 @@ module load compilers/gcc492
 module load mpi/openmpi-1.8.8
 module load cuda/Cuda-6.5
 
+python --version
+
 cd ${SOURCE_PREFIX}
 # Download the get-pip.py script
 wget -c https://bootstrap.pypa.io/get-pip.py
 # Download an SSL certificate
 # http://stackoverflow.com/a/26062583
 wget -c http://curl.haxx.se/ca/cacert.pem
-CERT_OPTIONS="--cert $SRC/cacert.pem"
-PYVERSION=$(python --version)
-echo "Python version is ${PYVERSION}"
+CERT_OPTIONS="--cert ${SOURCE_PREFIX}/cacert.pem"
 # --install
-python ${SOUCE_PREFIX}/get-pip.py install --install-option="--install-scripts=${INSTALL_PREFIX}/bin" ${CERT_OPTIONS}
+python ${SOURCE_PREFIX}/get-pip.py install ${CERT_OPTIONS}
+python -m pip --version
 # Upgrade pip
-python -m pip install -U --install-option="--install-scripts=${INSTALL_PREFIX}/bin" ${CERT_OPTIONS} pip 
+python -m pip install -U pip ${CERT_OPTIONS}
 # install numpy
-python -m pip install -U numpy ${CERT_OPTIONS}
+python -m pip install -U numpy ${CERT_OPTIONS} 
+# install virtualenv
+python -m pip install -U virtualenv ${CERT_OPTIONS}
